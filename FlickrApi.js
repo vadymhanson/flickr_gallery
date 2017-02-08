6b6e9510fafd27790107de2adb19a4953867bca7
vadymz <vadymz@migfin.com>
2017-02-08 17:05:42 +0200

    Annotaitons was added

--- FlickrApi.js
+++ FlickrApi.js
@@ -1,10 +1,23 @@
+/**
+ * Class for Flickr API requests
+ */
 class FlickrApi {
+    /**
+     * Constructor
+     * @param {string} _appkey
+     * @param {Ajax} _ajax
+     */
     constructor(_appkey, _ajax) {
         this.appkey = _appkey;
         this.ajax = new _ajax;
     }
+
+    /**
+     * Method that sends GET request to Flickr API
+     * @param {string} text
+     * @returns {Promise}
+     */
     searchPhotos(text) {
         return this.ajax.getData('https://api.flickr.com/services/rest/?method=flickr.photos.search&text=' + text + '&api_key=' + this.appkey + '&per_page=18&format=json&nojsoncallback=1');
     }
-    
 }
